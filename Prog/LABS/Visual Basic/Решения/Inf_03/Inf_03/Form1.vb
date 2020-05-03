0c8ce74e81bd4817d14966bc873e3a4dd7274c10
Elobor <IzotovK97@gmail.com>
2020-05-03 13:49:38 +0300

    Add files via upload

---
+++ Prog/LABS/Visual Basic/Решения/Inf_03/Inf_03/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+
+	Dim I As Integer
+	Dim R As Single
+	Dim str1, str2 As String
+
+	Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+		I = TextBox1.Text
+		R = TextBox2.Text
+		Label1.Text = I + R
+	End Sub
+
+	Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+		str1 = TextBox3.Text
+		str2 = TextBox4.Text
+		Label2.Text = str1 + str2
+	End Sub
+
+	Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+		str1 = TextBox5.Text
+		str2 = TextBox6.Text
+		Label3.Text = str1 + str2 + 25
+	End Sub
+
+End Class
